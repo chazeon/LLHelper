dcba6aa6756679177e0dc1c64d60a39bee979f07
lwxcd <60499101+lwxcd@users.noreply.github.com>
2021-07-10 15:23:11 +0800

    为默认曲目添加了新的谱面
    为默认曲目添加了更高combo的谱面

--- static/siteversion.js
+++ static/siteversion.js
@@ -3,7 +3,7 @@
  */
 
 var LLSiteVersion = (function(){
-   var current_version = 20210522;
+   var current_version = 20210710;
    var VISITED_VERSION_KEY = 'llhelper_visited_version__';
 
    function getVisitedVersion() {
