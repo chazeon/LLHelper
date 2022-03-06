1d8048add689c4e2b1b1ab27640aaf971d9f2c49
ben1222 <ben_281493815@qq.com>
2022-03-06 16:32:10 +0800

    add release notes

--- static/siteversion.js
+++ static/siteversion.js
@@ -3,7 +3,7 @@
  */
 
 var LLSiteVersion = (function(){
-   var current_version = 20220131;
+   var current_version = 20220306;
    var VISITED_VERSION_KEY = 'llhelper_visited_version__';
 
    function getVisitedVersion() {
